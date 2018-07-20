a1546d967eb467a8d11786797e9f4e3c0b3d2c0a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-20 00:05:24 -0400

    control form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,26 +2,47 @@ import React from 'react'
 
 class SignUpForm extends React.Component {
   state = {
+    username: "",
+    email: "",
+    password: "",
+    confirmation: ""
+  }
 
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onChange={this.onChange}>
         <label htmlFor="username">Username</label>
-        <input id="username" name="username" 
+        <input 
+          id="username" 
+          name="username" 
+          type="text"
           value={this.state.username}
         /> 
         <label htmlFor="email">Email</label>
-        <input id="email" name="email"
+        <input 
+          id="email" 
+          name="email"
+          type="text"
           value={this.state.email}
         />
         <label htmlFor="password">Password</label>
-        <input id="password" name="password"
+        <input 
+          id="password" 
+          name="password"
+          type="password"
           value={this.state.password}
         />
-        <label htmlFor="confirmation">Password</label>
-        <input id="confirmation" name="confirmation"
+        <label htmlFor="confirmation">Password Confirmation</label>
+        <input 
+          id="confirmation" 
+          name="confirmation"
+          type="password"
           value={this.state.confirmation}
         />
       </form>
