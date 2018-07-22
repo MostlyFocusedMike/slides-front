70f04e04d310404c662a1b4b7439fda21fab2591
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-22 18:46:57 -0400

    give login form its own state and control the form

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -7,17 +7,52 @@ class LogInForm extends React.Component {
     this.initState = {
       user: {
         username: "",
-        email: "",
-        password: "",
-        password_confirmation: ""
+        password: ""
       }
     }
     this.state = this.initState
   }
+
+  handleChange = (e) => {
+    this.setState({
+      user: {
+      ...this.state.user,
+      [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  handleClear = (e) => {
+    e.preventDefault()
+    this.setState(this.initState)
+  }
+
   render() {
     return (
-      <form>
-        <label>Hello</label>
+      <form
+        onChange={this.handleChange} 
+        onSubmit={this.handleSubmit}
+      >
+        <label>Username</label>
+        <input 
+          type="text"
+          name="username"
+          id="username"
+          value={this.state.user.username}
+        />
+        <label>Password</label>
+        <input 
+          type="text"
+          name="password"
+          id="password"
+          value={this.state.user.password}
+        />
+        <button>Submit</button>
+        <button onClick={this.handleClear}>Clear</button>
       </form>
     )
   }
