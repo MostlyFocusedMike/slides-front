5dc20da87a48402113e152d41fdbf50618039b19
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-23 00:16:56 -0400

    turn video page into class

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const VideoPage = (props) => {
-  return (
-    <div>
-      <h1>VideoPage</h1>
-    </div>
-  )
+class VideoPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>VideoPage</h1>
+      </div>
+    )
+  }
 }
 export default VideoPage
