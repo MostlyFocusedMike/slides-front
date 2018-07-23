4d70032bfc93c1734321e8b1f55777c7af25be5d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-23 13:58:10 -0400

    render different sections differently

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -2,18 +2,26 @@ import React from 'react'
 
 class SectionCard extends React.Component {
   createSection() {
-    const {kind, content, desc, "show_desc": showDesk} = this.props
-    // switch (kind) {
-    //   case 0:
-    //   case 1:
-    //   case 2:
-    //   case 3:
-    // }
+    const {kind, content, desc, "show_desc": showDesk} = this.props.section
+    console.log(content);
+    let text = ""
+    switch(kind) {
+      case 0:
+        return <p>{content}</p>;
+      case 1:
+        return <pre>{content}</pre>
+      case 2:
+        return <a href={content}>{desc}</a>
+      case 3:
+        return <img src={content} alt={desc} />
+    }
   }
+
   render() {
-    console.log("props", this.props);
+    console.log("props", this.props)
     return (
       <div>
+        {this.createSection()}
       </div>
     )
   }
