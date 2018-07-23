b407c9cc60f04130125a7cb02598b3bd9e7366d9
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-22 21:38:34 -0400

    make user page a class

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -2,13 +2,15 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {logOut} from '../store'
 
-const UserPage = (props) => {
-  return (
-    <div>
-      <h1>{props.currentUser.username}</h1>
-      <button onClick={props.logOut}>Log Out</button> 
-    </div>
-  )
+class UserPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.currentUser.username}</h1>
+        <button onClick={this.props.logOut}>Log Out</button> 
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => ({
