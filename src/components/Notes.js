91971b8482b9862e3ed3568c4baca0973612f382
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-23 10:54:22 -0400

    get ugly rough slide selector to work as time changes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -17,8 +17,22 @@ class Notes extends React.Component {
    return obj ? obj : Data.notes[Data.notes.length -1]
   }
 
+  pickSlide = () => {
+    let vid = this.props.realData.self[this.props.videoId]
+    let {time} = this.props
+    const {slides, sections} = this.props.realData
+    let selectedSlideId = vid.slides.find((slideId, idx, slideIds) => {
+      let nextIndex = slideIds.indexOf(slideId) + 1
+      let nextSlideId = slideIds[nextIndex] || slideIds[slideIds.length -1]
+      let nextSlide = slides[nextSlideId]
+      return time < nextSlide.start
+    })
+    return selectedSlideId ? slides[selectedSlideId] : slides[vid.slides[vid.slides.length - 1]]
+  }
+
   displayNote = () => {
     let note = this.pickNote() 
+    console.log("selected slide", this.pickSlide());
     if (note.type === "code") {
       return <pre>{note.content}</pre>
     } else if (note.type === "text") {
