dd00d368fed6fdaad3d79488759335eacba4216a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-22 22:18:15 -0400

    build out basic user attributes from test fetch

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -3,16 +3,41 @@ import {connect} from 'react-redux'
 import {logOut} from '../store'
 
 class UserPage extends React.Component {
+  state = {
+    user: {
+      id: 0,
+      username: "loading",
+      bio: "loading",
+      pic_link: "loading",
+      videos: []
+    }, 
+    shouldLoad: false
+  }
   componentDidMount() {
-    console.log(this.props);
+    console.log(this.props.match.params);
+    fetch(`http://localhost:3000/users/${this.props.match.params.username}`)
+      .then(r=>r.json())
+      .then(user => {
+        this.setState({
+          user,
+          shouldLoad: true
+        }, () => console.log(this.state))
+      })
   }
   render() {
-    return (
-      <div>
-        <h1>{this.props.currentUser.username}</h1>
-        <button onClick={this.props.logOut}>Log Out</button> 
-      </div>
-    )
+    const {username, bio, "pic_link": picLink} = this.state.user
+    if (this.state.shouldLoad) {
+      return (
+        <div>
+          <img src={picLink} />
+          <h1>{this.state.user.username}</h1>
+          <h2>{this.state.user.bio}</h2>
+          <button onClick={this.props.logOut}>Log Out</button> 
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 }
 
