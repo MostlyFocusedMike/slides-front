ff48992f64daf8c22ee3ff35272f82839d187ed5
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-24 10:19:08 -0400

    make state for new video page, this will be passed down to the various forms on the page

--- src/pages/NewVideoPage.js
+++ src/pages/NewVideoPage.js
@@ -1,10 +1,48 @@
 import React from 'react'
 
-const NewVideoPage = (props) => {
-  return (
-    <div>
-      <h1>New Video Page</h1>
-    </div>
-  )
+class NewVideoPage extends React.Component {
+  state = {
+    slideId: 1,
+    sectionId: 1, //video id will always be 0 for these forms, but slides and sections need to increment
+    entities: {
+      videos: {
+        0: {
+          id: 0,
+          desc: "Video description here",
+          youtube_vid: "",
+          topics: {selected: [], others: []},
+          // user: this.props.currentUser
+          // slides: [0] // not sure if i actually need this, we'll see
+        }
+      },
+      slides: {
+        0: {
+          id: 86,
+          video_id: 0,
+          start: 0,
+          title: "Slide One Title Here"
+        }
+      },
+      sections: {
+        0: {
+          id: 0,
+          slide_id: 0,
+          kind: 0,
+          order: 0,
+          content: "Content here",
+          desc: "Description here",
+          show_desc: false
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>New Video Page</h1>
+      </div>
+    )
+  }
 }
 export default NewVideoPage
