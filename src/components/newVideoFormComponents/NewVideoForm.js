e6fb5e072d07a7baefa0fc5c398e40791f61e25a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-08-01 10:13:09 -0400

    add in time converting functions for testing and playing with

--- src/components/newVideoFormComponents/NewVideoForm.js
+++ src/components/newVideoFormComponents/NewVideoForm.js
@@ -18,186 +18,34 @@ import {
 } from '../../store';
 
 class NewVideoForm extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.slideId = 1,
-  //   this.sectionId = 1, //video id will always be 0 for these forms, but slides and sections need to increment
-  //   this.state = {
-  //     fireRedirect: null,
-  //     loadPreview: false,
-  //     entities: {
-  //       videos: {
-  //         0: { 
-  //           id: 0,
-  //           desc: "",
-  //           youtube_vid: "",
-  //           topics: {selected: [], others: []},
-  //           user: props.currentUser,
-  //           slides: [0] 
-  //         }
-  //       },
-  //       slides: {
-  //         0: {
-  //           id: 0,
-  //           video_id: 0,
-  //           start: 0,
-  //           title: "",
-  //           sections: [0]
-  //         }
-  //       },
-  //       sections: {
-  //         0: {
-  //           id: 0,
-  //           slide_id: 0,
-  //           kind: 0,
-  //           order: 1,
-  //           content: "",
-  //           desc: "",
-  //           show_desc: false
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  //
-  // handleLoadPreview = (e) => {
-  //   e.preventDefault()
-  //   this.setState({
-  //     loadPreview: true
-  //   })
-  // }
-  //
-  // handleVideoChange = (e) => {
-  //   let loadPreview = (e.target.name === "youtube_vid") ?
-  //     e.target.value.length >= 11 : this.state.loadPreview
-  //   this.setState({
-  //     loadPreview,
-  //     entities: {
-  //       ...this.state.entities,
-  //       videos: {
-  //         ...this.state.entities.videos,
-  //         0: {
-  //           ...this.state.entities.videos[0],
-  //           [e.target.name]: e.target.value
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
-  //
-  // handleSlideChange = (e, id) => {
-  //   this.setState({
-  //     entities: {
-  //       ...this.state.entities,
-  //       slides: {
-  //         ...this.state.entities.slides,
-  //         [id]: {
-  //           ...this.state.entities.slides[id],
-  //           [e.target.dataset.key]: e.target.value
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
-  //
-  // handleSectionChange = (e, id) => {
-  //   const value = e.target.type === 'checkbox' ? 
-  //     e.target.checked : e.target.value;
-  //   this.setState({
-  //     entities: {
-  //       ...this.state.entities,
-  //       sections: {
-  //         ...this.state.entities.sections,
-  //         [id]: {
-  //           ...this.state.entities.sections[id],
-  //           [e.target.dataset.key]: value
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
-  //
-  // handleFormSubmit = (e) => {
-  //   e.preventDefault()
-  //   videoAdapter.create(this.state.entities)
-  //     .then(videoId => {
-  //       this.setState({fireRedirect: videoId.id}) 
-  //     })
-  // }
-  //
-  // handleFieldSubmit = (e) => {
-  //   // fieldsets seem to not submit forms, and activate inputs
-  //   e.preventDefault()
-  // }
-  //
-  // newSlide = (e) => {
-  //   e.preventDefault() 
-  //   this.setState({
-  //     entities: {
-  //       ...this.state.entities,
-  //       videos: {
-  //         0: {
-  //           ...this.state.entities.videos[0],
-  //           slides: [...this.state.entities.videos[0].slides,
-  //                   this.slideId]
-  //         }
-  //       },
-  //       slides: {
-  //         ...this.state.entities.slides,
-  //         [this.slideId]: {
-  //           id: this.slideId,
-  //           video_id: 0,
-  //           start: this.slideId,
-  //           title: "",
-  //           sections: []
-  //         }
-  //       }
-  //     }
-  //   })
-  //   this.slideId++
-  // }
-  //
-  // newSection = (e, slideId) => {
-  //   e.preventDefault()
-  //   // this.sectionId is coming from the form constructor, slideId from the actual section
-  //   const {slides, sections} = this.state.entities
-  //   this.setState({
-  //     entities: {
-  //       ...this.state.entities,
-  //       slides: {
-  //         ...slides,
-  //         [slideId]: {
-  //           ...slides[slideId], 
-  //           sections: [...slides[slideId].sections,
-  //                      this.sectionId]
-  //         }
-  //       },
-  //       sections: {
-  //         ...sections,
-  //         [this.sectionId]: {
-  //           id: this.sectionId,
-  //           slide_id: slideId,
-  //           kind: 0,
-  //           order: slides[slideId].sections.length +1,
-  //           content: "",
-  //           desc: "",
-  //           show_desc: false
-  //         }
-  //       }
-  //     }
-  //   })
-  //   this.sectionId++
-  // }
-  //
-    //
- 
- handleSubmit = (e) => {
-   e.preventDefault()
- }
- 
- componentDidUpdate = (prevProps) => {
-   if (prevProps.currentUser.id !== this.props.currentUser.id) {
-     this.props.setVideoUser(this.props.currentUser)
+  state = {
+    slideStart: "0:00"
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+   
+  secondsToHMS(input) {
+      let parts = this.state.slideStart.split(':'),
+          seconds = parts[parts.length - 1],
+          minutes = parts[parts.length - 2] || 0,
+          hours = parts[parts.length - 3] || 0;
+      return (hours * 3600 + minutes * 60 + seconds)
+  }
+
+  function secondsToHms(d) {
+      d = Number(d);
+
+      var h = Math.floor(d / 3600);
+      var m = Math.floor(d % 3600 / 60);
+      var s = Math.floor(d % 3600 % 60);
+
+      return (h) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.currentUser.id !== this.props.currentUser.id) {
+      this.props.setVideoUser(this.props.currentUser)
     }
   }
 
@@ -215,8 +63,10 @@ class NewVideoForm extends React.Component {
           youtube_vid={youtube_vid}
           desc={desc}
         />
-        <SlidesContainer />
+        <label> Make a new slide at </label>
+        <input value={this.state.slideStart} onChange={(e) => this.setState({slideStart: e.target.value})}/> 
         <button onClick={this.props.newSlide}>Make new slide</button>
+        <SlidesContainer />
         <button>Create Video Project</button>
 
       </form>
